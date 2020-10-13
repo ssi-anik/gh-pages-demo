674348aad8d0330b6e34fcc615e4bfca49d26c36
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-13 17:41:37 +0000

    Deploy ssi-anik/gh-pages-demo to github.com/ssi-anik/gh-pages-demo.git:gh-pages

---
+++ precache-manifest.144dc3d07be052c413d156893ab72b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a0656cda9537d69113f784cdb2429f",
+    "url": "/gh-pages-demo/index.html"
+  },
+  {
+    "revision": "f3fc07a1819c2b179334",
+    "url": "/gh-pages-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a797c86ea3188a89cde9",
+    "url": "/gh-pages-demo/static/js/2.f96a093f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-demo/static/js/2.f96a093f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3fc07a1819c2b179334",
+    "url": "/gh-pages-demo/static/js/main.8ed240fb.chunk.js"
+  },
+  {
+    "revision": "54d62c5b7904e5001992",
+    "url": "/gh-pages-demo/static/js/runtime-main.c8642553.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
